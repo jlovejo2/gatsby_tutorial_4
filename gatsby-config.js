799c04d63e150c91e9c33a5b7e89af2a3c84bcf7
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-14 19:59:46 -0500

    queried the file structure of a page

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,16 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: "Title from siteMetadata",
+    title: "Panda Eating Lots",
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
