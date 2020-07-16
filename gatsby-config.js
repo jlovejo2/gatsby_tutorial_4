7570b48b39a45f8f24dd81c68c8e2b2e642e425d
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-16 10:21:33 -0500

    set-up app to run as PWA, manifest, service-worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,9 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: "Panda Eating Lots",
+    title: `Panda Eating Lots`,
+    description: `A simple description about pandas eating lots...`,
+    author: `gatsbyJs_tutorial`,
   },
   plugins: [
     {
@@ -25,5 +27,21 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
   ],
 }
